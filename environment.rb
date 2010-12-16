ba4b2651d727ee49afb51599128d33c556be0db0
Keith Norman <keithn@groupon.com>
2010-12-16 11:52:33 -0600

    initial commit

--- environment.rb
+++ environment.rb
@@ -1,9 +1,4 @@
 require 'rubygems'
-require 'dm-core'
-require 'dm-timestamps'
-require 'dm-validations'
-require 'dm-aggregates'
-require 'dm-migrations'
 require 'haml'
 require 'ostruct'
 
@@ -19,8 +14,5 @@ configure do
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
-
-               
-  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
 
 end
